6281ea06601ae9afa857cb3117cb1ae7b4a117a7
AndreyUA <infernalsteel666@gmail.com>
2023-05-28 13:43:30 +0300

    created plane and added grid

--- src/js/script.js
+++ src/js/script.js
@@ -20,13 +20,25 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const axesHelper = new THREE.AxesHelper(6);
 scene.add(axesHelper);
 
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, material);
 scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+const gridHelper = new THREE.GridHelper(30, 100);
+scene.add(gridHelper);
 
 const animate = (time) => {
   box.rotation.x = time / 1_000;
